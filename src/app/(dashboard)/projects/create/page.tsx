986f9879ce183eb89ec43e6096e5f7fbb6c6c943
Yas246 <bigwabi01@gmail.com>
2024-06-20 00:25:23 +0100

    up

--- src/app/(dashboard)/projects/create/page.tsx
+++ src/app/(dashboard)/projects/create/page.tsx
@@ -2,14 +2,12 @@
 
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import CreateEditProject from "@/components/CreateEditForm/Project";
-import React from "react";
 
 export default function Create() {
-
-    return (
-        <div>
-            <Breadcrumb pageName="Créer projet" />
-            <CreateEditProject />
-        </div>
-    );
+  return (
+    <div>
+      <Breadcrumb pageName="Créer Mission" />
+      <CreateEditProject />
+    </div>
+  );
 }
